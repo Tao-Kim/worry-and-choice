931e3b58c28d18d4a893df5ccf5b047b57fe6546
Tao-Kim <taog397@gmail.com>
2020-06-29 01:02:26 +0900

    Add 레이아웃 추가

--- app/src/main/java/com/tao/wnc/MainActivity.java
+++ app/src/main/java/com/tao/wnc/MainActivity.java
@@ -3,12 +3,31 @@ package com.tao.wnc;
 import android.os.Bundle;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 
 public class MainActivity extends AppCompatActivity {
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
+        transaction.add(R.id.main_container, ListFragment.newInstance()).commit();
     }
+
+    public void removeAndPop(Fragment fragment){
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        fragmentManager.beginTransaction().remove(fragment).commit();
+        fragmentManager.popBackStack();
+    }
+
+    public void replaceWithBackStack(Fragment fragment){
+        getSupportFragmentManager().beginTransaction().replace(R.id.main_container, fragment).addToBackStack(null).commit();
+    }
+
 }
