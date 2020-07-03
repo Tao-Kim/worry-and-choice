45789b7ac77c70168ff4529dd1bc70dd8bcb5e6c
Tao-Kim <taog397@gmail.com>
2020-07-03 15:45:17 +0900

    Revert "Update fragment_list"
    This reverts commit c0b92f20b5292dafc28d349644fe5dbaa713317a.

--- app/src/main/java/com/tao/wnc/view/ListFragment.java
+++ app/src/main/java/com/tao/wnc/view/ListFragment.java
@@ -8,20 +8,12 @@ import android.view.ViewGroup;
 import androidx.annotation.NonNull;
 import androidx.databinding.DataBindingUtil;
 import androidx.fragment.app.Fragment;
-import androidx.recyclerview.widget.LinearLayoutManager;
-import androidx.recyclerview.widget.RecyclerView;
 
 import com.tao.wnc.R;
-import com.tao.wnc.adapter.PostListAdapter;
 import com.tao.wnc.databinding.FragmentListBinding;
-import com.tao.wnc.viewmodel.ListViewModel;
 
 
 public class ListFragment extends Fragment {
-
-    private FragmentListBinding binding;
-    private ListViewModel viewModel;
-    private PostListAdapter adapter;
 
     public ListFragment() {
         // Required empty public constructor
@@ -39,40 +31,23 @@ public class ListFragment extends Fragment {
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        binding = DataBindingUtil.inflate(inflater, R.layout.fragment_list, container, false);
+        FragmentListBinding binding = DataBindingUtil.inflate(inflater, R.layout.fragment_list, container, false);
         binding.setFragment(this);
-
-        viewModel = new ListViewModel();
-
-        RecyclerView recyclerView = binding.rvList;
-        recyclerView.setLayoutManager(new LinearLayoutManager(getContext()));
-        adapter = new PostListAdapter();
-        recyclerView.setAdapter(adapter);
-
-        getList();
 
         return binding.getRoot();
     }
 
-    private void getList(){
-        adapter.setItems(viewModel.getListItems());
+    public void onMyPostsClick(View v){
+        ((MainActivity)getActivity()).replaceWithBackStack(MyPostsFragment.newInstance());
     }
-
-
-    public void onMyPostsClick(View v) {
-        ((MainActivity) getActivity()).replaceWithBackStack(MyPostsFragment.newInstance());
+    public void onAddPostClick(View v){
+        ((MainActivity)getActivity()).replaceWithBackStack(AddPostFragment.newInstance());
     }
-
-    public void onAddPostClick(View v) {
-        ((MainActivity) getActivity()).replaceWithBackStack(AddPostFragment.newInstance());
+    public void onReadPostClick(View v){
+        ((MainActivity)getActivity()).replaceWithBackStack(ReadPostFragment.newInstance());
     }
-
-    public void onReadPostClick(View v) {
-        ((MainActivity) getActivity()).replaceWithBackStack(ReadPostFragment.newInstance());
-    }
-
-    public void onNotificationsClick(View v) {
-        ((MainActivity) getActivity()).replaceWithBackStack(NotificationsFragment.newInstance());
+    public void onNotificationsClick(View v){
+        ((MainActivity)getActivity()).replaceWithBackStack(NotificationsFragment.newInstance());
     }
 
 
