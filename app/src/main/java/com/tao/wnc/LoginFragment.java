d4eaa31b036bf76e88054e368cf5d16e3101b574
Tao-Kim <taog397@gmail.com>
2020-07-03 15:45:17 +0900

    Revert "Update fragment_login.xml"
    This reverts commit c833b8253b78fcc8903be0878147f269d558659b.

--- app/src/main/java/com/tao/wnc/LoginFragment.java
+++ app/src/main/java/com/tao/wnc/LoginFragment.java
@@ -8,7 +8,6 @@ import android.view.ViewGroup;
 import androidx.databinding.DataBindingUtil;
 import androidx.fragment.app.Fragment;
 
-import com.bumptech.glide.Glide;
 import com.tao.wnc.databinding.FragmentLoginBinding;
 
 /**
@@ -17,6 +16,7 @@ import com.tao.wnc.databinding.FragmentLoginBinding;
  * create an instance of this fragment.
  */
 public class LoginFragment extends Fragment {
+
 
 
     private FragmentLoginBinding binding;
@@ -33,9 +33,6 @@ public class LoginFragment extends Fragment {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
-
-
     }
 
     @Override
@@ -44,17 +41,15 @@ public class LoginFragment extends Fragment {
         binding = DataBindingUtil.inflate(inflater, R.layout.fragment_login, container, false);
         binding.setFragment(this);
 
-        Glide.with(this).asGif().load(R.raw.login_background).into(binding.loginIvBackground);
-
         View root = binding.getRoot();
         return root;
     }
 
-    public void onLoginClick(View v) {
-        ((LoginActivity) getActivity()).goMainActivity();
+    public void onLoginClick(View v){
+        ((LoginActivity)getActivity()).goMainActivity();
     }
 
-    public void onRegisterClick(View v) {
+    public void onRegisterClick(View v){
         getActivity().getSupportFragmentManager().beginTransaction().replace(R.id.login_container, RegisterFragment.newInstance()).addToBackStack(null).commit();
     }
 }
