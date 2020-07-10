9bba97de95234f89b90b6731709949a8e1947361
Tao-Kim <taog397@gmail.com>
2020-07-10 11:08:21 +0900

    Update AddPost

---
+++ app/src/main/java/com/tao/wnc/viewmodel/AddPostViewModel.java
@@ -0,0 +1,39 @@
+package com.tao.wnc.viewmodel;
+
+import androidx.lifecycle.ViewModel;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.tao.wnc.model.domain.PostItem;
+import com.tao.wnc.model.domain.SelectItem;
+import com.tao.wnc.model.repository.PostRepository;
+import com.tao.wnc.model.repository.UserRepository;
+import com.tao.wnc.util.Constants;
+
+public class AddPostViewModel extends ViewModel {
+
+    private final static String TAG = AddPostViewModel.class.getName();
+    private FirebaseUser user;
+    private UserRepository userRepository;
+    private PostRepository postRepository;
+
+
+
+
+    public AddPostViewModel() {
+        user = FirebaseAuth.getInstance().getCurrentUser();
+        userRepository = new UserRepository();
+        postRepository = new PostRepository();
+    }
+
+    public void addItem(String title, String description, String selectAString, String selectBString) {
+        String writer = user.getDisplayName();
+        String timeStamp = null;
+        SelectItem selectA = new SelectItem(selectAString);
+        SelectItem selectB = new SelectItem(selectBString);
+        Short selected = Constants.SELECTED.NOT_SELECTED;
+
+        PostItem item = new PostItem(title, description, writer, timeStamp, selectA, selectB, selected);
+        postRepository.insertPost(item);
+    }
+}
